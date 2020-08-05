3d51f4cb1e2abe03c2bc676e7654b5ca983d6e75
Jordan Triplett <JordanT111596@gmail.com>
2020-08-04 22:18:22 -0400

    Added psuedocode and personal reminders

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+//create convenience variables for all html sections
+//create elements if needed
+//create question array
+
+//replace the starter info and button
+//append questions, variables, and buttons to the body
+
+//create button choices for selecting answer
+//create button to submit and go to next question
+//store answer as correct or incorrect locally
