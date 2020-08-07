f65700a5f68701c8fc64278cf262944ad489456b
Jordan Triplett <JordanT111596@gmail.com>
2020-08-07 00:31:46 -0400

    added comments

--- assets/scores.js
+++ assets/scores.js
@@ -1,11 +1,14 @@
+//Declares element variables
 var highScoresOlEl = document.querySelector("#scoresList");
 var resetBtn = document.querySelector("#reset");
 
+//How the scores show up on the screen
 function displayHighScores() {
     var localStorageHighScores = JSON.parse(window.localStorage.getItem("localStorageHighScores")) || [];
 
-    //can add sort()
+    //gotta add sort() somewhere, somehow..
 
+    //iterates through all scores from localStorage array
     for (var i = 0; i < localStorageHighScores.length; i++) {
         var liEl = document.createElement("li");
         liEl.textContent = localStorageHighScores[i].name + ": " + localStorageHighScores[i].score;
@@ -14,11 +17,14 @@ function displayHighScores() {
     }
 }
 
+//just clears the local storage
 function resetScores() {
     window.localStorage.clear();
     displayHighScores();
 }
 
-resetBtn.addEventListener("click", resetScores);
-
+//calls function to display scores
 displayHighScores();
+
+//gives button functionality
+resetBtn.addEventListener("click", resetScores);
--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-//create convenience variables for all html sections
+//create convenience variables for all html elements
 var timerEl = document.querySelector("#timer");
 var questionEl = document.querySelector("#question");
 var starterParagraphEl = document.querySelector("#starterParagraph");
@@ -8,11 +8,10 @@ var containerDivEl = document.querySelector(".container");
 var mainEl = document.querySelector("main");
 var nameInput = document.querySelector(".form-control");
 
+//set up global variables for various functions
 var timerInterval;
 var time = 100;
 var questionSetIndex = 0;
-
-//create elements if needed
 
 //create question array
 var questionSet = [
@@ -68,19 +67,25 @@ var questionSet = [
     }
 ]
 
-//Start the game
+//Start the button functionality
 startButtonEl.addEventListener("click", gameStart);
 
+//actually starts the game
 function gameStart() {
+    //Only if start button is hit
     if (event.target.matches("button")) {
+        //remove start button
         startButtonEl.remove();
+        //hide starter paragraph
         starterParagraphEl.setAttribute("style", "display: none !important");
 
+        //start countdown
         timerInterval = setInterval(tickDown, 1000)
         displayQuestions();
     }
 }
 
+//function for showing the question on the screen from array
 function displayQuestions() {
 
 
@@ -97,59 +102,82 @@ function displayQuestions() {
     }
 }
 
+//function for showing the answers on the screen from array of objects
 function setAnswers(indexVal) {
+    //creates button for each answer choice
     var answerBtn = document.createElement("button");
+    //sets answer choice text to appear in button
     answerBtn.textContent = questionSet[questionSetIndex].answerList[indexVal];
+    //set bootstrap css class for button
     answerBtn.setAttribute("class", "btn btn-primary m-2");
+    //set data attribute so we can check if it's the right answer later
     answerBtn.setAttribute("data-index", indexVal);
 
+    //give button functionality to check answer and progress
     answerBtn.addEventListener("click", checkAnswer);
 
+    //add button to the button group
     buttonGroupEl.appendChild(answerBtn);
 }
 
+//function for counting down clock/score
 function tickDown() {
+    //time/score decreases
     time--;
+    //updates timer in top corner
     timerEl.textContent = "Time left: " + time;
 
+    //end the game when time runs out
     if (time <= 0) {
         end();
     }
 }
 
+//function for checking if the answer is right and what to do from there
 function checkAnswer() {
+    //make sure there's no refresh due to the form
     event.preventDefault();
+    //compare answer to corect answer value, if correct move on
     if (parseInt(event.target.getAttribute("data-index")) === questionSet[questionSetIndex].correctAnswer) {
         questionSetIndex++;
     }
 
+    //if not correct, move on but with 10 less seconds
     else {
         time = time - 10;
         questionSetIndex++;
     }
+    //remove buttons so new buttons can have space to themselves
     buttonGroupEl.innerHTML = "";
 
+    //when you reach the end of the questions the game is over
     if (questionSetIndex === questionSet.length) {
         return end();
     }
+    //shows new set of questions if there's more to go
     displayQuestions()
 }
 
+//function for Game Over
 function end() {
     //Brings up screen for the intitals and save
     clearInterval(timerInterval);
 
+    //creates and appends form for name
     var nameBoxFormEl = document.createElement("form");
     buttonGroupEl.appendChild(nameBoxFormEl);
 
+    //creates, styles, and appends div for name
     var nameBoxEl = document.createElement("div");
     nameBoxEl.setAttribute("class", "form-group");
     nameBoxFormEl.appendChild(nameBoxEl);
 
+    //creates, styles, and appends label for name
     var nameLabel = document.createElement("label");
     nameLabel.setAttribute("for", "name-text");
     nameBoxEl.appendChild(nameLabel);
 
+    //creates, styles, and appends input for name
     var textForName = document.createElement("input");
     textForName.setAttribute("type", "text");
     textForName.setAttribute("placeholder", "Enter your name here!");
@@ -157,42 +185,49 @@ function end() {
     textForName.setAttribute("id", "name-text")
     nameBoxEl.appendChild(textForName);
 
+    //creates, styles, and appends button for submit
     var submitBtn = document.createElement("button");
     submitBtn.setAttribute("type", "submit");
     submitBtn.setAttribute("class", "btn btn-primary mb-2");
     submitBtn.textContent = "Submit";
     nameBoxEl.appendChild(submitBtn);
 
+    //added functionality for button to store scores upon click
     submitBtn.addEventListener("click", storeScore);
 
+    //Replaces question with some directions for the user
     questionEl.textContent = "Enter your name!";
 
 }
 
+//function for storing the scores in the local storage
 function storeScore() {
+    //make sure there's no refresh due to the form
     event.preventDefault();
+    //convenience variable for name input
     var textForName = document.querySelector("#name-text");
+    //make sure there's no filler in the name input
     var name = textForName.value.trim();
 
+    //if the name isn't empty, store it
     if (name) {
+        //creates object array for stored scores
         var localStorageHighScores = JSON.parse(window.localStorage.getItem("localStorageHighScores")) || [];
 
+        //the entry the use just made gets stored
         var newScore = {
             name: name,
             score: time
         }
 
+        //put that new entry into the array
         localStorageHighScores.push(newScore);
+        //store that array locally
         window.localStorage.setItem("localStorageHighScores", JSON.stringify(localStorageHighScores))
 
+        //redirect user to highscore page
         window.location.href = "assets/highscores.html";
     }
 
 
 }
-//replace the starter info and button
-//append questions, variables, and buttons to the body
-
-//create button choices for selecting answer
-//create button to submit and go to next question
-//store answer as correct or incorrect locally
